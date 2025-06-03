5534df5020be4865a5cd7657bbf954e2ba7ace56
Ned Kaida-Yip <ned.kaida-yip@spr.com>
2025-06-03 15:38:03 -0500

    check in before test restructure

--- YumTest/Properties/Resources.Designer.cs
+++ YumTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YumTest.Properties {
+namespace YumUnitTests.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace YumTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YumTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YumUnitTests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
---
+++ YumTest/SnapshotHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YumTest
+{
+    public class SnapshotHelper
+    {
+        public static string WrapWithStyles(string markup)
+        {
+            var cssPath = ResolveCssPath();
+
+            return @$"
+                <!DOCTYPE html>
+                <html lang=""en"">
+
+                    <head>
+                        <meta charset=""utf-8"" />
+                        <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
+                        <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"" crossorigin=""anonymous"">
+                        <link rel=""stylesheet"" href=""https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"">
+                        <link rel=""stylesheet"" href=""{cssPath}/app.css"" />
+                    </head>
+                    <body>
+                        <div class=""review-container"">
+                            {markup}
+                        </div>
+                    </body>
+
+                </html>
+            ";
+        }
+
+        private static string ResolveCssPath()
+        {
+            // Strategy 1: Check if wwwroot was copied to output
+            if (Directory.Exists("wwwroot"))
+                return "wwwroot";
+
+            // Strategy 2: Relative path to main project
+            // C:\work\Yum\YumTest\bin\Debug\net9.0
+            if (Directory.Exists("../../../../wwwroot"))
+                return "../../../wwwroot";
+
+            return string.Empty;
+        }
+    }
+}
